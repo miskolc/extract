6755669854c82f8b16bcce4902090fd43ed12362
miskolc <calinho_gx1992@yahoo.com>
2012-09-19 00:00:16 +0300

    Making the application work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,32 @@
 class StaticPagesController < ApplicationController
+  include ApplicationHelper
+  require 'open-uri'
   def home
+    link = params[:link] 
+    if !link.nil?
+        if !link.empty?
+            # link_to_id extracts the id or returns nil if there is no id 
+            id= link_to_id(link)
+            if !id.nil?
+                    val = ""
+
+                    # Building the GDATA URI and extracting the XML
+                    query = "http://gdata.youtube.com/feeds/api/videos?q=" + id
+                    open(query) { |f| 
+                        f.each_line { |line| val = val + line }
+                    }
+
+                    # If there are 0 results in the GDATA API then there is no video
+                    not_found = val.scan('totalResults>0<')
+                    if not_found.empty?
+                            flash.now[:success] = "Youtube video was found!"
+                        else 
+                            flash.now[:error] = "Youtube video not found!" 
+                    end        
+            else 
+                    flash.now[:notification] = "Please type a Youtube link"        
+            end
+        end
+    end         
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+    def link_to_id( link )
+        if link.nil?
+            link=""
+        end    
+       length=link.length-1
+       a=nil
+       (1..length).map do |i|
+           if link[i]=='=' && link[i-1]=='v' 
+              a=i+1
+           end
+           break if !a.nil?
+         end
+        
+        # if the link does have a the substring 'v='
+        # and there are at least 11 chars after it return the YT id
+        
+        if !a.nil? && a+10 <= length
+            link[a..a+10]
+        else 
+            nil
+        end        
+
+    end
 end
